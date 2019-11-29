bed838d0200cf09587dd10a70aa12ed1067db25f
Jakub Dubovsky <jakub.dubovsky@seznam.cz>
2019-11-29 15:25:03 +0100

    parametrize

--- build.sc
+++ build.sc
@@ -4,9 +4,10 @@ import coursier.maven.MavenRepository
 object reckon extends ScalaModule {
   def scalaVersion = "2.13.1"
   def ivyDeps = Agg(
-   ivy"org.ajoberstar.reckon:reckon-core:0.12.0",
-   ivy"com.github.zafarkhaja:java-semver:0.9.0",
-   ivy"org.apache.commons:commons-lang3:3.9"
+    ivy"org.ajoberstar.reckon:reckon-core:0.12.0",
+    ivy"com.github.zafarkhaja:java-semver:0.9.0",
+    ivy"org.apache.commons:commons-lang3:3.9",
+    ivy"com.github.alexarchambault::case-app:2.0.0-M9"
   )
   def repositories = super.repositories ++ Seq(
     MavenRepository("https://jcenter.bintray.com/")
--- reckon/src/james64/ReckonStandalone.scala
+++ reckon/src/james64/ReckonStandalone.scala
@@ -2,23 +2,33 @@ package james64
 
 import java.util.Optional
 
+import caseapp._
+
 import org.ajoberstar.reckon.core.Reckoner
 import org.eclipse.jgit.lib.Repository
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder
 
-object ReckonStandalone {
+object ReckonStandalone extends CaseApp[Options] {
 
-  def main(args: Array[String]) : Unit = println {
+  def run(options: Options, arg: RemainingArgs): Unit = println {
     Reckoner.builder()
-      .git(localGit)
-      .scopeCalc(_ => Optional.ofNullable(args(0)))
-      .stageCalc((_,_) => Optional.ofNullable(args(1)))
-      .stages(args(1))
+      .git(localGit(options.gitDir))
+      .scopeCalc(_ => Optional.ofNullable(options.scope.orNull))
+      .stageCalc((_,_) => Optional.ofNullable(options.stage.orNull))
+      .stages(dummyStage(options.stage))
       .build
       .reckon
   }
 
-  private def localGit : Repository = (new FileRepositoryBuilder())
-    .setGitDir(new java.io.File(".git"))
+  private def dummyStage(input: Option[String]) : String = input match {
+    case None => "rc"
+    case Some("final") => "rc"
+    case Some(s) => s
+  }
+
+  private def localGit(gitDir: String) : Repository = new FileRepositoryBuilder()
+    .setGitDir(new java.io.File(gitDir))
     .build()
 }
+
+case class Options(scope: Option[String], stage: Option[String], gitDir: String = ".git")
